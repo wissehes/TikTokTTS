5b03581dbc044c9727c85dccc79cc749da1adbee
Wisse <wissehes@gmail.com>
2023-03-15 13:09:48 +0100

    Initial Commit

---
+++ TikTokTTS/TikTokTTSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokTTSApp.swift
+//  TikTokTTS
+//
+//  Created by Wisse Hes on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TikTokTTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
